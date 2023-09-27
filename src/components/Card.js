028c3c43f796c638c9b9a8449858d1b5206dc9f8
FahimaGold <df_mokhtari@esi.dz>
2023-09-27 12:39:17 +0100

    add global style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import { CardContainer, Img } from './CardStyle'
+import { HeaderTitle, H3Title, PText } from './styles/GlobalStyle'
 export default function Card(props){
     return(
        
@@ -8,9 +9,15 @@ export default function Card(props){
            <Img src={props.imageUrl}/>
                
               <span ><FontAwesomeIcon icon={faMapMarkerAlt} size="lg" style={{color: '#F55A5A',}} />&nbsp;&nbsp;&nbsp;{props.location.toUpperCase()}&nbsp;&nbsp;&nbsp;<a href={props.googleMapsUrl} style={{width: '200px'}}>View on Google Maps</a></span>
-               <h1>{props.title}</h1>
-               <h3>{props.startDate}-{props.endDate}</h3>
-               <p>{props.description}</p>
+               <HeaderTitle>
+                {props.title}
+                </HeaderTitle>
+               <H3Title>
+                {props.startDate}-{props.endDate}
+                </H3Title>
+               <PText>
+                {props.description}
+                </PText>
            </CardContainer >
            
         
--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -15,7 +15,22 @@ export const HeaderTitle = styled.h1`
   }
 `;
 
-export const BoxText = styled.p`
+export const H3Title = styled.h3`
+  display: 'flex';
+  marginRight: '10px';
+  justify-content: center;
+  font-size: 1rem;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    font-size: 1rem;
+  }
+  
+
+
+`;
+
+export const PText = styled.p`
   margin-top: ${px2vw(20)};
   color: #666;
   font-size: 1.5rem;
