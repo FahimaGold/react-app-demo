e97317826a0d2045810b25f72966459717ff8e74
FahimaGold <df_mokhtari@esi.dz>
2023-09-27 09:21:13 +0100

    seperate style from logic Card component

---
+++ src/components/CardStyle.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import px2vw from "../Utils/px2vw";
+
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: stretch;
+  margin: ${px2vw(32)};
+  width: 100%;  
+  @media (max-width: 767px) {
+    flex-direction: column;
+    align-items: stretch;
+    width: 100%; 
+    height: auto; 
+  }
+`;
+
+export const Img = styled.img`
+
+  display: flex;
+  flex-direction: row;
+  width: ${px2vw(320)};
+  height: ${px2vw(200)};
+  
+  padding: ${px2vw(20)};
+  margin: ${px2vw(20)};
+ 
+  @media (max-width: 767px) {
+    display: flex;
+   flex-direction: column;
+    width: 100%; 
+    height: auto;
+    
+  }
+
+  
+`;
+
+
+export const BoxTitle = styled.h3`
+  color: #333;
+  font-size: 2rem;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const BoxText = styled.p`
+  margin-top: ${px2vw(20)};
+  color: #666;
+  font-size: 1.5rem;
+
+  @media (min-width: 1024px) {
+    font-size: 1rem;
+  }
+`;
--- src/components/pages/Travels.js
+++ src/components/pages/Travels.js
@@ -5,9 +5,15 @@ import data from '../../data'
 const Travels = () => {
     const cards = data.map((item) =>{
         return (
+            <div style={{display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            
+            }}>
        <Card key={item.id} 
        {...item}
        />
+       </div>
         )
     } )
     return (
